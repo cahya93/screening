2c79bde873c5b152f88a89e8c811ae1359344bc4
Eko Cahyanto <49233072+cahya93@users.noreply.github.com>
2021-03-16 14:06:32 +0700

    update data

--- application/views/admin/rekap-kategori.php
+++ application/views/admin/rekap-kategori.php
@@ -33,7 +33,7 @@
                         <td width="150px"><?= $d['nama']; ?></td>
                         <td>
                             <?php
-                            $score = $d['p1'] + $d['p2'] + $d['p3'] + $d['p4'] + $d['p5'] + $d['p6'] + $d['p7']+$d['p8']+$d['p9']+$d['p10'];
+                            $score = $d['p1'] + $d['p2'] + $d['p3'] + $d['p4'] + $d['p5'] + $d['p6'] + $d['p7'] + $d['p8'] + $d['p9'] + $d['p10'];
                             // echo $score;
                             if ($score <= 60) {
                                 echo "<span class='btn btn-danger'>$score</span>";
@@ -46,12 +46,11 @@
                         </td>
                         <td>
                             <?php
-                            // $score = $d['p1'] + $d['p2'] + $d['p3'] + $d['p4'] + $d['p5'] + $d['p6'] + $d['p7'];
                             if ($score <= 60) {
                                 echo "<span class='btn btn-danger'>Stop</span>";
-                            } else if ($score < 75) {
+                            } else if ($score < 89) {
                                 echo "<span class='btn btn-warning'>Waspada</span>";
-                            } else if ($score >= 75) {
+                            } else if ($score >= 90) {
                                 echo "<span class='btn btn-success'>Aman</span>";
                             }
                             ?>
